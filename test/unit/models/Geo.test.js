ccba1f9ac95d0531ef8d3da64eedd2594cce709e
xscheiner <xscheiner@gmail.com>
2016-04-30 11:55:48 +0100

    geo model and tests

--- test/unit/models/Geo.test.js
+++ test/unit/models/Geo.test.js
@@ -1,8 +1,8 @@
 import test from 'tape';
 import sinon from 'sinon';
-import {city as sql} from '../../../config/sql.json';
+import {geo as sql} from '../../../config/sql.json';
 import {models} from '../../../config/messages.json';
-import Model from '../../../lib/models/City';
+import Model from '../../../lib/models/Geo';
 
 let mocks;
 
@@ -19,31 +19,18 @@ const setMocks = () => {
   mocks.id = 'fake id';
 };
 
-before('++++++++++++ Setting up City model tests ++++++++++++', (assert) => {
+before('++++++++++++ Setting up Geo model tests ++++++++++++', (assert) => {
   mocks = {};
   assert.end();
 });
 
-test('*** List cities ***', (assert) => {
-
-  assert.test('Sends the query', (assert) => {
-
-    setMocks();
-    let city = new Model(mocks.mapper);
-    city.list(mocks.id, mocks.done);
-    assert.equal(mocks.mapper.client.query.calledWith(sql.list, [mocks.id], mocks.done), true);
-    assert.end();
-  });
-  assert.end();
-});
-
-test('Cities bounds query', (assert) => {
+test('Geo bounds query', (assert) => {
 
   assert.test('Valid params', (assert) => {
     setMocks();
     let params = ['1.2345', '2.6543', '3.7654', '4.4567'];
-    let city = new Model(mocks.mapper);
-    city.bounds(params, mocks.done);
+    let geo = new Model(mocks.mapper);
+    geo.bounds(params, mocks.done);
     assert.equal(mocks.mapper.client.query.calledWith(sql.bounds, params, mocks.done), true);
     assert.end();
   });
@@ -51,8 +38,8 @@ test('Cities bounds query', (assert) => {
   assert.test('Insufficient number of params in request', (assert) => {
     setMocks();
     let params = ['1.234', '2.345', '3.456'];
-    let city = new Model(mocks.mapper);
-    city.bounds(params, mocks.done);
+    let geo = new Model(mocks.mapper);
+    geo.bounds(params, mocks.done);
     assert.equal(mocks.mapper.client.query.called, false);
     assert.equal(mocks.done.calledWith(models.bounds['400']), true);
     assert.end();
@@ -61,8 +48,8 @@ test('Cities bounds query', (assert) => {
   assert.test('One or more params are not numbers/floats', (assert) => {
     setMocks();
     let params = ['1.234', '2.345', '3.456', 'fake'];
-    let city = new Model(mocks.mapper);
-    city.bounds(params, mocks.done);
+    let geo = new Model(mocks.mapper);
+    geo.bounds(params, mocks.done);
     assert.equal(mocks.mapper.client.query.called, false);
     assert.equal(mocks.done.calledWith(models.bounds['422']), true);
     assert.end();
@@ -71,7 +58,7 @@ test('Cities bounds query', (assert) => {
   assert.end();
 });
 
-after('++++++++++++ Cleaning up after City model tests ++++++++++++', (assert) => {
+after('++++++++++++ Cleaning up after Geo model tests ++++++++++++', (assert) => {
   mocks = null;
   assert.end();
 });
